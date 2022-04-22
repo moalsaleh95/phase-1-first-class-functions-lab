8265b9c9e9f8a539429e83b0ba5d9398538715a1
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2022-04-23 00:27:02 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (array) {
+    return array.slice(0, 2);
+}
+// console.log(returnFirstTwoDrivers(drivers));
+
+const returnLastTwoDrivers = function (array) {
+    return array.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+// selectingDrivers[0](drivers);
+// selectingDrivers[1](drivers);
+// console.log(selectingDrivers);
+
+function createFareMultiplier(fare) {
+    return function () {
+        return fare * 5;
+    }
+}
+
+function fareDoubler(fare) {
+    createFareMultiplier(fare);
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    createFareMultiplier(fare);
+    return fare * 3;
+}
+
+function selectDifferentDrivers(array, func) {
+    if (func === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(array)
+    }
+    else if (func === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(array)
+    }
+}
